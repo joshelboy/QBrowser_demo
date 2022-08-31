ad5f71d73049a4a0a19db689001517c41fb3783b
joshelboy <elias_tilegant@gmx.de>
2022-08-31 10:29:38 +0200

    alert

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,5 @@ function BarcodeData(barcode, type, typeText) {
     document.getElementById("type") = type;
     document.getElementById("typeText") = typeText;
     document.getElementById("barcode") = barcode;
+    alert(barcode);
 }
