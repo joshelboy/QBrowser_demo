5118e9e9668f90397d0544754529a8b0b5970443
joshelboy <elias_tilegant@gmx.de>
2022-08-31 10:05:19 +0200

    DOM manipulation

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 function BarcodeData(barcode, type, typeText) {
-    PrintOutput("Type(" + type + "):" + typeText + " Barcode: " + barcode + "<br/>")
-    //document.getElementById("type") = type;
-    //document.getElementById("typeText") = typeText;
-    //document.getElementById("barcode") = barcode;
+    //PrintOutput("Type(" + type + "):" + typeText + " Barcode: " + barcode + "<br/>")
+    document.getElementById("type") = type;
+    document.getElementById("typeText") = typeText;
+    document.getElementById("barcode") = barcode;
 }
