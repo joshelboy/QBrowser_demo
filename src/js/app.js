e24febeeb4a6b30c4894e2bd8e3abdf2c5ed72b2
joshelboy <elias_tilegant@gmx.de>
2022-08-31 11:20:23 +0200

    final commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,23 @@
 function BarcodeData(barcode, type, typeText) {
     //PrintOutput("Type(" + type + "):" + typeText + " Barcode: " + barcode + "<br/>")
-    document.getElementById("type") = type;
-    document.getElementById("typeText") = typeText;
-    document.getElementById("barcode") = barcode;
-    alert(barcode);
+    //document.body.innerHTML += type + "<br/>";
+    //document.body.innerHTML += typeText + "<br/>";
+    //document.body.innerHTML += barcode + "<br/>";
+    document.getElementById("type").innerHTML = type;
+    document.getElementById("typeText").innerHTML = typeText;
+    document.getElementById("barcode").innerHTML = barcode;
+    document.getElementById("barcodeField_automatic").value = barcode;
+    QBrowser.Notify.vibrate();
+}
+
+function BarcodeStatus(info)
+{
+    if (info.connected)
+        document.body.innerHTML += "Connected barcode reader <b>" + info.name + " " + info.version + "</b>";
+    else
+        document.body.innerHTML += "Barcode reader disconnected<br/>";
+}
+
+function BarcodeScan(){
+    QBrowser.Barcode.startScan();
 }
