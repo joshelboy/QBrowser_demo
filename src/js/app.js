86aee6660ab113e622ded85a573c1bce03c77bc9
joshelboy <elias_tilegant@gmx.de>
2022-08-31 09:58:09 +0200

    added print on site

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 function BarcodeData(barcode, type, typeText) {
-    PrintOutput("Type(" + type + "):" + typeText + " Barcode: " + barcode + "<br/>")
+    document.getElementById("type") = type;
+    document.getElementById("typeText") = typeText;
+    document.getElementById("barcode") = barcode;
 }
